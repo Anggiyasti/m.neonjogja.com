39eb635b75f13c0938d2fc5e6e69a28616d31b6a
nursofia09 <nur.sofia@mail.unpas.ac.id>
2017-04-11 09:51:42 +0700

    jjj

--- welcome/controllers/welcome.php
+++ welcome/controllers/welcome.php
@@ -89,13 +89,10 @@ class Welcome extends MX_Controller {
 
         );
 
-<<<<<<< HEAD
+
         if ($this->session->userdata('penggunaID')) {
           
-=======
 
-
->>>>>>> 447a082e7a438802d7629b3b09d8e67f5787538f
         $data['files'] = array( 
 
             // APPPATH.'modules/homepage/views/v-header-login.php',
@@ -103,10 +100,6 @@ class Welcome extends MX_Controller {
             APPPATH.'modules/welcome/views/v-welcome.php',
             APPPATH.'modules/templating/views/layouts/v-footer.php',
 
-<<<<<<< HEAD
-=======
-            
->>>>>>> 447a082e7a438802d7629b3b09d8e67f5787538f
 
             // APPPATH.'modules/welcome/views/v-tampil-tes.php',
 
@@ -114,14 +107,13 @@ class Welcome extends MX_Controller {
 
         );
 
-<<<<<<< HEAD
+
         }
         else{
             redirect('login');
         }
 
-=======
->>>>>>> 447a082e7a438802d7629b3b09d8e67f5787538f
+
         $data['tingkat'] = $this->load->MTingkat->gettingkat();
 
         // print_r($data['tingkat']);
